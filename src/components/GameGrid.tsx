570a59ca976343b8ea63a0d4e7337bde3d4082c4
Anirudh Jwala <anirudhsai2@gmail.com>
2023-07-03 03:37:11 +0530

    Code refactoring: Remvoed duplicated card styles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,7 +1,12 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
+
+// Hooks
 import useGames from '../hooks/useGames';
+
+// Components
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
+import GameCardContainer from './GameCardContainer';
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
@@ -16,9 +21,15 @@ const GameGrid = () => {
         spacing={10}
       >
         {isLoading &&
-          skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
+          skeletons.map(skeleton => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
         {games.map(game => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
