14c2ba98e26410eb505bc49a1de9b40c7bc0fc23
Anirudh Jwala <anirudhsai2@gmail.com>
2023-07-06 21:21:40 +0530

    Grid fixed card size issue resolved Added template based columns Genre list with images and names Updated Genre model

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,27 @@
+import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
+
 import useGenres from '../hooks/useGenres';
+import getCroppedImageUrl from '../services/image-urls';
 
 const GenreList = () => {
   const { data: genres } = useGenres();
 
   return (
-    <ul>
+    <List>
       {genres.map(genre => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+              fit="cover"
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
